ee7abb9c620c24dc7c0acf76722463d2b2558706
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-15 12:49:16 +0000

    update valid email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,26 @@
+
+DELIMITER //
+
+CREATE TRIGGER update_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    DECLARE is_valid_email INT DEFAULT 0;
+    
+    IF NEW.email <> OLD.email THEN
+        -- Perform email validation logic here
+        -- Set is_valid_email to 1 if the email is valid
+        
+        -- Example email validation logic using a simple regular expression
+        IF NEW.email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' THEN
+            SET is_valid_email = 1;
+        END IF;
+    ELSE
+        -- If the email is not changed, retain the existing valid_email value
+        SET is_valid_email = OLD.valid_email;
+    END IF;
+    
+    SET NEW.valid_email = is_valid_email;
+END //
+
+DELIMITER ;
