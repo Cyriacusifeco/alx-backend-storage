2666295f6a2d726dadf80c3e6e7cb9c2bf9afb29
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-19 17:33:21 +0000

    read from redis and recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     """
@@ -28,3 +28,46 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """
+        Retrieves the value from Redis for the given key.
+
+        Args:
+            key: The key associated with the data in Redis.
+            fn: A callable function to convert the data back to the desired format (Optional).
+
+        Returns:
+            Union[str, bytes, int, float]: The data retrieved from Redis, optionally converted using fn.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves the value from Redis for the given key and converts it to a UTF-8 string.
+
+        Args:
+            key: The key associated with the data in Redis.
+
+        Returns:
+            str: The data retrieved from Redis, converted to a UTF-8 string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves the value from Redis for the given key and converts it to an integer.
+
+        Args:
+            key: The key associated with the data in Redis.
+
+        Returns:
+            int: The data retrieved from Redis, converted to an integer.
+        """
+        return self.get(key, fn=int)
